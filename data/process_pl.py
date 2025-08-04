07aec9a812a9ac9d7842a3512d8e70c1b11cb1aa
Sein Jone <seinjonetan@gmail.com>
2025-08-04 10:16:35 -0700

    feat: year cutoff  definitions and mapping update

--- data/process_pl.py
+++ data/process_pl.py
@@ -5,6 +5,7 @@ import glob
 import os
 import re
 
+#  Define mappings for occupations based on OCC1990DD codes
 mappings = [
     ((405, 408), 'occ3_clean'),
     ((415, 427), 'occ3_protect'),
@@ -36,12 +37,14 @@ mappings = [
     ((991, 991), 'unemployed'),
 ]
 
+# Helper function to map OCC1990DD codes to occupations
 def map_occupations(occ1990dd):
     for (start, end), occupation in mappings:
         if start <= occ1990dd <= end:
             return occupation
     return 'other'
 
+# Function to expand ranges in sector names for mapping
 def expand_ranges(df):
     expanded_rows = []
     for index, row in df.iterrows():
@@ -53,14 +56,17 @@ def expand_ranges(df):
             expanded_rows.append((index, row['Name']))
     return pd.DataFrame(expanded_rows, columns=['Sector', 'Name']).set_index('Sector')
 
+# Scan initial data
 print('Reading data...')
-df = pl.scan_csv('raw/usa_00019.csv')
+df = pl.scan_csv('raw/usa_00020.csv')
 
+# Map occupations
 print('Mapping occpations...')
 df = df.with_columns(
     pl.col('OCC1990').map_elements(map_occupations, return_dtype=pl.Utf8).alias('occupation')
 )
 
+# Convert area mappings and read in crosswalks
 print('Creating area mappings...')
 df_cz_1990 = pd.read_csv('raw/cw_puma1990_czone.csv', encoding='latin1')
 df_cz_2000 = pd.read_csv('raw/cw_puma2000_czone.csv', encoding='latin1')
@@ -70,6 +76,7 @@ df = df.with_columns(
     (pl.col('STATEFIP').cast(pl.Int32) * 1000 + pl.col('CNTYGP98').cast(pl.Int32)).alias('ctygrp1980'),
 )
 
+# Standardize NAICS codes and create sector mappings
 print('Creating sector mappings...')
 df = df.with_columns(
     pl.col('INDNAICS').cast(pl.Utf8).str.slice(0, 2).alias('INDNAICS')
@@ -84,19 +91,28 @@ naics_codes.loc['50'] = 'Transportation and Warehousing'
 naics_codes.loc['3M'] = 'Manufacturing'
 naics_codes = expand_ranges(naics_codes)
 
+naics_cw = pd.read_csv('raw/crosswalk/naics_1990.csv')
+naics_cw['naics2'] = naics_cw['naics6'].astype(str).str[:2]
+naics_cw = naics_cw[['naics2', 'ind1990']]
+naics_cw = naics_cw.drop_duplicates(subset=['naics2', 'ind1990'])
+
+# Extract unique years from the dataset
 years = df.filter(pl.col('YEAR').is_not_null()).select(pl.col('YEAR').unique()).collect().to_series()
 # years = pd.Series(years)[years >= 2010].to_list()
 
 total_pop = []
 city_rent_all = pd.DataFrame()
 
+# Define the area for pivot tables
 area = 'COMZONE'
 
+# process each year
 for year in tqdm(years, desc='Generating datasets: '):
     print(f'Processing data for year {year}...')
     print('Filtering data...')
     current = df.filter(pl.col('YEAR') == year).collect().to_pandas()
 
+    # Multiply income and rent by household weight
     current['INCWAGE'] = current['INCWAGE'] * current['HHWT']
     current['AVERAGE INCWAGE'] = current['INCWAGE'] / current['HHWT']
     current['RENT'] = current['RENT'] * current['HHWT']
@@ -104,8 +120,8 @@ for year in tqdm(years, desc='Generating datasets: '):
     current['HH_RENT'] = current['RENT'].notnull().astype(int)
     current['HH_RENT'] = current['HH_RENT'] * current['HHWT']
 
+    # Apply area mappings
     print('Applying maps...')
-    current['INDNAICS'] = current['INDNAICS'].map(naics_codes['Name'])
     # current['COMZONE'] = current['FIPS'].map(df_cz.set_index('FIPS')['County Name'])
     if year == 1990:
         current = current.merge(df_cz_1990, left_on='FIPS', right_on='puma1990', how='left')
@@ -117,6 +133,14 @@ for year in tqdm(years, desc='Generating datasets: '):
         current = current.merge(df_cz_2000, left_on='FIPS', right_on='puma2000', how='left')
         current['COMZONE'] = current['czone']
 
+    # For years before 1990, apply NAICS crosswalks
+    if year <= 1990:
+        print('Applying NAICS crosswalks...')
+        current = current.merge(naics_cw, left_on='IND1990', right_on='ind1990', how='left')
+        current['INDNAICS'] = current['naics2']
+    current['INDNAICS'] = current['INDNAICS'].map(naics_codes['Name'])
+
+    # Pivot tables for employment, wages, and rent
     city_occ = current.pivot_table(index=area, columns='occupation', values='HHWT', aggfunc='sum')
     # city_occ_wage = current.pivot_table(index=area, columns='occupation', values='AVERAGE INCWAGE', aggfunc='mean')
     city_sector = current.pivot_table(index=area, columns='INDNAICS', values='HHWT', aggfunc='sum')
@@ -130,6 +154,7 @@ for year in tqdm(years, desc='Generating datasets: '):
     city_pop = current.pivot_table(index=area, values='HH_RENT', aggfunc='sum')
     city_rent[f'{year}'] = city_rent['RENT'] / city_pop['HH_RENT']
 
+    # City rents to be merged across years
     if city_rent_all.empty:
         city_rent_all = city_rent[f'{year}']
         city_rent_all = pd.DataFrame(city_rent_all)
@@ -143,6 +168,7 @@ for year in tqdm(years, desc='Generating datasets: '):
     except:
         pass
 
+    # save datasets
     print('Saving datasets...')
     city_occ.to_csv(f'processed/city_occ_employment/city_occ_e_{year}.csv')
     # city_occ_wage.to_csv(f'processed/city_occ_wage/city_occ_w_{year}.csv')
@@ -155,11 +181,13 @@ for year in tqdm(years, desc='Generating datasets: '):
     print('Dumping cache...')
     del current, city_occ, city_sector, city_rent, city_pop
 
+# Save total population and city rent data
 total_pop = pd.DataFrame(total_pop)
 total_pop.to_csv('processed/total_pop.csv', index=False)
 city_rent_all.to_csv('processed/city_rent.csv')
 print('Success!')
 
+# Process TFP data
 print('Processing TFP data...')
 tfp = pd.read_excel('raw/tfp.xlsx', header=1, skiprows=1)
 tfp = tfp[tfp['Measure'] == 'Total factor productivity']
@@ -172,6 +200,7 @@ tfp.index = tfp.index.map(naics_codes['Name'])
 tfp = tfp.groupby(tfp.index, axis=0).mean()
 tfp.to_csv('output/tfp.csv')
 
+# Get comzone crosswalk
 print('Creating master dataset...')
 df_cw = pd.read_csv('raw/cw_puma2000_czone.csv', encoding='latin1')
 # df_names = pd.read_csv('raw/puma_names.csv')
@@ -200,6 +229,7 @@ def get_data(directory, field_name, id_vars=['COMZONE'], var_name='Occupation'):
 
     return data
 
+# Load employment data and merge with crosswalk
 employment = get_data('processed/city_occ_employment/*.csv', 'Employed')
 employment = employment.merge(df_cw, on='COMZONE', how='left')
 employment = employment[['Year', 'COMZONE', 'NAME', 'Occupation', 'Employed']]
@@ -212,10 +242,12 @@ occupation_counts = final.groupby('Occupation')['Year'].nunique()
 valid_occupations = occupation_counts[occupation_counts == total_years].index
 final = final[final['Occupation'].isin(valid_occupations)]
 
+# Load sector employment and wage bill data
 city_sec_employment = get_data('processed/city_sec_employment/*.csv', 'Employed', id_vars=['COMZONE'], var_name='Sector')
 sec_occ_wb = get_data('processed/sec_occ_wb/*.csv', 'Wage_Bill', id_vars=['INDNAICS'], var_name='Occupation')
 sec_occ_wb = sec_occ_wb[sec_occ_wb['Occupation'].isin(valid_occupations)]
 
+# CPI data to rebase prices
 df_cpi = pd.read_csv('raw/CPI.csv')
 df_cpi['DATE'] = pd.to_datetime(df_cpi['DATE'])
 df_cpi['Year'] = df_cpi['DATE'].dt.year
@@ -235,6 +267,7 @@ sec_occ_wb['Wage_Bill'] = (sec_occ_wb['Wage_Bill'] / sec_occ_wb['CPI']) * df_cpi
 
 print('Saving datasets...')
 
+# Save final datasets
 final.to_csv('processed/master.csv', index=False)
 city_sec_employment.to_csv('processed/city_sec_employment.csv', index=False)
 sec_occ_wb.to_csv('processed/sec_occ_wb.csv', index=False)
